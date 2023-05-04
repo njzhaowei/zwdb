7fd51178af6e7ffb082f1e4118ba3049b618a854
njzhaowei <yewberry@126.com>
2023-05-04 08:55:35 +0800

    add sqlite

--- zwdb/zwsqlite.py
+++ zwdb/zwsqlite.py
@@ -1,63 +1,67 @@
-import os
-from contextlib import contextmanager
-
-import mysql.connector
-import mysql.connector.pooling
-
-from . import utils
+import sqlite3
+import traceback
+import logging
 from .records import RecordCollection
 
-class ZWMysql(object):
-    """Class defining a MySQL driver"""
+class ZWSqlite(object):
+    """Class defining a sqlite driver"""
     def __init__(self, db_url, **kwargs):
-        self.db_url = db_url or os.environ.get('DATABASE_URL')
-        if not self.db_url:
-            raise ValueError('You must provide a db_url.')
-
-        o = utils.db_url_parser(db_url)
-        p = o['props']
+        # file:C://test.db or test.db
+        self.isuri = True if db_url.startswith('file:') else False
         self.dbcfg = {
-            'host'      : o['host'],
-            'port'      : o['port'] or 3306,
-            'user'      : o['usr'],
-            'password'  : o['pwd'],
-            'database'  : o['db'],
-            'charset'           : p.get('characterEncoding', 'utf8mb4'),
-            'use_unicode'       : p.get('useUnicode', True),
-            'connect_timeout'   : p.get('connectTimeout', 10),
         }
+        self.dburl = db_url
         self.dbcfg.update(kwargs)
-        cfg = {
-            'collation' : 'utf8mb4_general_ci',
-            'use_pure'  : True,
-            'pool_size' : 5
+        cfgdef = {
         }
-        for k,v in cfg.items():
+        for k, v in cfgdef.items():
             self.dbcfg[k] = self.dbcfg.get(k, v)
-        self._debug = False
-        self._pool = None
+        self.debug = self.dbcfg.get('debug', False)
 
-    pool_size = property(lambda self: self._pool.pool_size)
-    version = property(lambda _: mysql.connector.__version__)
+    @property
+    def version(self):
+        '''Return sqlite_version'''
+        return sqlite3.sqlite_version
+
+    @property
+    def info(self):
+        '''Return version and sqlite_version'''
+        return {
+            'sqlite_version': sqlite3.sqlite_version,
+            'version': sqlite3.version,
+        }
 
     def get_connection(self):
-        if not self._pool:
-            self._pool = mysql.connector.pooling.MySQLConnectionPool(**self.dbcfg)
-        conn = self._pool.get_connection()
-        return ZWMysqlConnection(conn, debug=self._debug)
+        conn = sqlite3.connect(self.dburl, uri=self.isuri)
+        return ZWSqliteConnection(conn, debug=self.debug)
 
     def close(self):
-        if self._pool:
-            # pylint: disable=protected-access
-            self._pool._remove_connections()
+        pass
 
     def lists(self):
+        '''List all tables in database'''
         with self.get_connection() as conn:
-            rs = conn.execute('SHOW TABLES')
-            recs = rs.all()
+            recs = conn.execute("SELECT name FROM sqlite_master WHERE type='table' order by name", fetchall=True)
         return recs
 
     def find(self, tbl, clause=None, fetchall=False, **params):
+        '''Select records from table
+
+        :param str tbl: table name
+        :param dict clause: clause setting such as LIMIT, ORDER BY
+        :param bool fetchall: fetch all or not
+        :param dict params: select where condition
+        :return: result set
+        :rtype: :py:class:`zwtk.sqlite.RecordCollection`
+
+        .. code-block:: Python
+            :linenos:
+
+            db.find('tbl', clause={'ORDER BY num': 'DESC'})
+            db.find('tbl', none=None, txt={'like': 'txt%'}, num={'<>': 2})
+            db.find('tbl', num={'range': (1, 3)}) # 1<=num<3
+            db.find('tbl', num={'or': (2, 3)})
+        '''
         conn = self.get_connection()
         recs = conn.find(tbl, clause, fetchall, **params)
         if fetchall:
@@ -65,42 +69,158 @@ class ZWMysql(object):
         return recs
 
     def findone(self, tbl, clause=None, **params):
+        '''Select one record from table
+
+        :param str tbl: table name
+        :param dict clause: clause setting such as LIMIT, ORDER BY
+        :param dict params: select where condition
+        :return: result record or None
+        :rtype: :py:class:`zwtk.sqlite.Record`
+
+        .. code-block:: Python
+            :linenos:
+
+            db.findone('tbl', clause={'ORDER BY num': 'DESC'} num={'>': 1})
+        '''
         clause = clause or {}
         clause['limit'] = 1
         recs = self.find(tbl, clause=clause, fetchall=True, **params)
         return recs[0] if len(recs)>0 else None
 
     def exists(self, tbl, rec=None, keyflds=None, **params):
+        '''Check existence of record
+
+        :param str tbl: table name
+        :param `zwtk.sqlite.Record`/dict rec: record object to check
+        :param list(str) keyflds: key field list
+        :param dict params: select where condition
+        :return: exist or not
+        :rtype: bool
+
+        .. code-block:: Python
+            :linenos:
+
+            recs = {'id':1, 'txt': 'txt1', 'num': 1 }
+            db.exists('tbl', recs, keyflds=['id'])
+        '''
         with self.get_connection() as conn:
             rtn = conn.exists(tbl, rec, keyflds, **params)
         return rtn
 
     def count(self, tbl, **params):
+        '''Get table count by where condition in params
+
+        :param str tbl: table name
+        :param dict params: select where condition
+        :return: count
+        :rtype: int
+        '''
         with self.get_connection() as conn:
             rtn = conn.count(tbl, **params)
         return rtn
 
     def insert(self, tbl, recs):
+        '''Insert recs into table, return insert count
+
+        :param str tbl: table name
+        :param list(dict{str, object}) recs: record dict list
+        :return: insert count
+        :rtype: int
+
+        .. code-block:: Python
+            :linenos:
+
+            recs = [
+                {'txt': 'txt1', 'num': 1, 'dt': datetime.now()},
+                {'txt': 'txt2', 'num': 2, 'dt': datetime.now()},
+            ]
+            db.insert('tbl', recs)
+        '''
         with self.get_connection() as conn:
             rtn = conn.insert(tbl, recs)
         return rtn
 
     def update(self, tbl, recs, keyflds):
+        '''Update recs in table, return update count
+
+        :param str tbl: table name
+        :param list(dict{str, object}) recs: record dict list
+        :param list(str) keyflds: key field(s) list used to locate records
+        :return: update count
+        :rtype: int
+
+        .. code-block:: Python
+            :linenos:
+
+            recs = [
+                {'id':1, 'txt': '1txt', 'num': 1.5 },
+            ]
+            db.update('tbl', recs, keyflds=['id'])
+        '''
         with self.get_connection() as conn:
             rtn = conn.update(tbl, recs, keyflds)
         return rtn
 
     def upsert(self, tbl, recs, keyflds):
+        '''Update(if exists)/Insert(if not) recs in table, return update/insert count
+
+        :param str tbl: table name
+        :param list(dict{str, object}) recs: record dict list
+        :param list(str) keyflds: key field(s) list used to locate records
+        :return: (insert_count, update_count)
+        :rtype: tuple
+
+        .. code-block:: Python
+            :linenos:
+
+            recs = [
+                {'id':1, 'txt': 'txt1', 'num': 1 },
+                {'id':4, 'txt': 'txt4', 'num': 4 },
+                {'id':5, 'txt': 'txt5', 'num': 5 },
+            ]
+            db.upsert('tbl', recs, keyflds=['id'])
+        '''
         with self.get_connection() as conn:
             rtn = conn.upsert(tbl, recs, keyflds)
         return rtn
 
     def delete(self, tbl, recs=None, keyflds=None, **params):
+        '''Delete recs by record list or where condition, return delete count
+
+        :param str tbl: table name
+        :param list(dict{str, object}) recs: record dict list
+        :param list(str) keyflds: key field(s) list used to locate records
+        :param dict params: select where condition
+        :return: delete
+        :rtype: int
+
+        .. code-block:: Python
+            :linenos:
+
+            recs = [
+                {'id':4, 'txt': 'txt4', 'num': 4 },
+            ]
+            db.delete('tbl', recs, keyflds=['id'])
+            db.delete('tbl', dt=None)
+        '''
         with self.get_connection() as conn:
             rtn = conn.delete(tbl, recs, keyflds, **params)
         return rtn
 
     def select(self, stmt, fetchall=True, **params):
+        '''Run raw select sql
+
+        :param str stmt: sql statement
+        :param bool fetchall: fetch all records
+        :param dict params: select where condition
+        :return: result set
+        :rtype: :py:class:`zwtk.sqlite.RecordCollection`
+
+        .. code-block:: Python
+            :linenos:
+
+            db.select('SELECT txt FROM tbl WHERE id=1')
+        '''
         conn = self.get_connection()
         rtn =  conn.execute(stmt, fetchall=fetchall, **params)
         if fetchall:
@@ -108,41 +228,38 @@ class ZWMysql(object):
         return rtn
 
     def exec_script(self, fp):
-        with self.get_connection() as conn:
-            # pylint: disable=protected-access
-            cursor = conn._conn.cursor()
-            statement = ''
-            with open(fp, encoding='utf-8') as fs:
-                for line in fs:
-                    if line.strip().startswith('--'):  # ignore sql comment lines
-                        continue
-                    if not line.strip().endswith(';'):  # keep appending lines that don't end in ';'
-                        statement = statement + line
-                    else:  # when you get a line ending in ';' then exec statement and reset for next statement
-                        statement = statement + line
-                        cursor.execute(statement)
-                        statement = ''
+        '''Run sql script file
+
+        :param str fp: sql file path
+        :return: success or not
+        :rtype: bool
+
+        .. code-block:: Python
+            :linenos:
+
+            db.exec_script('C:/data.sql')
+        '''
+        try:
+            with self.get_connection() as conn:
+                statement = ''
+                with open(fp, encoding='utf-8') as fs:
+                    for line in fs:
+                        line = line.strip()
+                        if line.startswith('--'):  # ignore sql comment lines
+                            continue
+                        if not line.endswith(';'):  # keep appending lines that don't end in ';'
+                            statement = statement + line
+                        else:  # when you get a line ending in ';' then exec statement and reset for next statement
+                            statement = statement + line
+                            conn.execute(statement)
+                            statement = ''
+        except Exception:
+            logging.error(traceback.format_exc())
+            return False
         return True
 
-    @contextmanager
-    def transaction(self):
-        """A context manager for executing a transaction on this Database."""
-        conn = self.get_connection()
-        conn.transaction = True
-        _conn = conn.conn
-        _conn.autocommit = False
-        _conn.start_transaction()
-        try:
-            yield conn
-            _conn.commit()
-        except Exception:
-            _conn.rollback()
-        finally:
-            conn.transaction = False
-            conn.close()
-
     def __repr__(self):
-        return '<Database host={}:{}>'.format(self.dbcfg['host'], self.dbcfg['port'])
+        return '<Database dburl={}>'.format(self.dburl)
 
     def __enter__(self):
         return self
@@ -150,13 +267,14 @@ class ZWMysql(object):
     def __exit__(self, exc, val, traceback):
         self.close()
 
-class ZWMysqlConnection(object):
+class ZWSqliteConnection(object):
     conn = property(lambda self: self._conn)
 
     def __init__(self, conn, debug=False):
         self._conn = conn
         self._cursor = None
         self.open = True
+
         self.transaction = False
         self._debug = debug
 
@@ -168,8 +286,13 @@ class ZWMysqlConnection(object):
 
     def close(self):
         self._close_cursor()
-        self._conn.close()
+        self._close_conn()
         self.open = False
+
+    def _close_conn(self):
+        if self._conn:
+            self._conn.close()
+            self._conn = None
 
     def _close_cursor(self):
         if self._cursor:
@@ -193,9 +316,8 @@ class ZWMysqlConnection(object):
             print('%s <= %s'%(stmt, params))
         params = params or {}
         # Execute the given query
-        self._cursor = self._conn.cursor(buffered=False)
-        self._cursor.execute(stmt, params=params)
-        keys = self._cursor.column_names
+        self._cursor = self._conn.execute(stmt, params)
+        keys = [o[0] for o in self._cursor.description] if self._cursor.description else None
         if commit:
             self._conn.commit()
         row_gen = (keys, self)
@@ -209,9 +331,8 @@ class ZWMysqlConnection(object):
             print('%s <= %s'%(stmt, paramslist))
         paramslist = paramslist or []
         # Execute the given query
-        self._cursor = self._conn.cursor(buffered=False)
-        self._cursor.executemany(stmt, paramslist)
-        keys = self._cursor.column_names
+        self._cursor = self._conn.executemany(stmt, paramslist)
+        keys = [o[0] for o in self._cursor.description] if self._cursor.description else None
         if commit:
             self._conn.commit()
         row_gen = (keys, self)
@@ -221,8 +342,6 @@ class ZWMysqlConnection(object):
         return results
 
     def find(self, tbl, clause=None, fetchall=False, **params):
-        """select query
-        """
         stmt = 'SELECT * FROM {}'.format(tbl)
         if params:
             vs = self._get_wheres(**params)
@@ -258,7 +377,7 @@ class ZWMysqlConnection(object):
             return 0
         ks = recs[0].keys()
         fs = ','.join(ks)
-        vs = ','.join(['%({})s'.format(s) for s in ks])
+        vs = ','.join([':{}'.format(s) for s in ks])
         stmt = 'INSERT INTO {} ({}) VALUES({})'.format(tbl, fs, vs)
         commit = not self.transaction
         rc = self.executemany(stmt, paramslist=recs, commit=commit, fetchall=False)
@@ -269,7 +388,7 @@ class ZWMysqlConnection(object):
             return 0
         rec = recs[0]
         ks = rec.keys()
-        vs = ','.join(['{0}=%({0})s'.format(s) for s in ks])
+        vs = ','.join(['{0}=:{0}'.format(s) for s in ks])
         ws = self._get_keyflds(keyflds)
         stmt = 'UPDATE {} SET {} WHERE {}'.format(tbl, vs, ws)
         commit = not self.transaction
@@ -306,9 +425,17 @@ class ZWMysqlConnection(object):
         rc = self.executemany(stmt, paramslist=recs, commit=commit, fetchall=False)
         return rc._rows._cursor.rowcount
 
+    def _get_wheres(self, **params):
+        ks = params.keys()
+        ws = [self._cond_map({k:params[k]}) if any([isinstance(params[k], dict), params[k] is None]) else '{0}=:{0}'.format(k) for k in ks]
+        # ws = ['{0}=%({0})s'.format(k) if not any([isinstance(params[k], dict), params[k] is None]) else self._cond_map({k:params[k]}) for k in ks]
+        ws.append('1=1')
+        ws = ' AND '.join(ws)
+        return ws
+
     def _cond_map(self, o):
         hm = {
-            None: 'ISNULL(%s)',
+            None: '%s IS NULL',
             'like': '%s LIKE %s',
             '<>': '%s <> %s',
             '>': '%s > %s',
@@ -339,14 +466,7 @@ class ZWMysqlConnection(object):
         return s
 
     def _get_keyflds(self, keyflds):
-        ws = ['{0}=%({0})s'.format(k) if isinstance(k, str) else self._cond_map(k) for k in keyflds]
-        ws.append('1=1')
-        ws = ' AND '.join(ws)
-        return ws
-
-    def _get_wheres(self, **params):
-        ks = params.keys()
-        ws = ['{0}=%({0})s'.format(k) if not any([isinstance(params[k], dict), params[k] is None]) else self._cond_map({k:params[k]}) for k in ks]
+        ws = ['{0}=:{0}'.format(k) if isinstance(k, str) else self._cond_map(k) for k in keyflds]
         ws.append('1=1')
         ws = ' AND '.join(ws)
         return ws
